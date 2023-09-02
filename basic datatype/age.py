6e6c97dcd053e21fe9219ac926fe50b7834ab984
Agalya204 <agalya107@gmail.com>
2023-09-02 19:26:49 +0530

    git add.

---
+++ basic datatype/age.py
@@ -0,0 +1,9 @@
+name=input("Enter your name: ")
+print("Hello,",name)
+birth_year = int(input("What year you were born? "))
+current_year = 2023
+age = current_year - birth_year
+print("You are", age, "years old")
+#Your first number is 50. Give a different number
+
+    
